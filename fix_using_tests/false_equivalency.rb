024afc562d27c6d43b4f6b1d5afa5b10b07f639a
Akiko Green <akikogreen08@gmail.com>
2020-04-24 00:23:13 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,9 +11,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
     return "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
     return "HAM HAM HAM IN MY TUMMY"
   end
    binding.pry
