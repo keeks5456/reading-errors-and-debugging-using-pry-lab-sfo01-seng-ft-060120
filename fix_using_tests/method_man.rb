af8b45dd147efee4b3aa68afd0614c0dcda842ec
Akiko Green <akikogreen08@gmail.com>
2020-04-24 00:30:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,9 +2,9 @@
 
 def start_game(player1, player2)
 # binding.pry
-puts "Hello #{player1} & #{playar2}"
+return "Hello #{player1} & #{playar2}"
 end
 
 def play_game(player1, player2)
- puts "#{player1} is better than #{player2}"
+ return "#{player1} is better than #{player2}"
 end
