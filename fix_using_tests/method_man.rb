a20ee9f836048dcb63e3390070b980cfd0c4252c
Akiko Green <akikogreen08@gmail.com>
2020-04-24 00:28:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,8 @@
  require 'pry'
 
 def start_game(player1, player2)
-# puts "Hello #{player1} & #{playar2}"
+# binding.pry
+puts "Hello #{player1} & #{playar2}"
 end
 
 def play_game(player1, player2)
