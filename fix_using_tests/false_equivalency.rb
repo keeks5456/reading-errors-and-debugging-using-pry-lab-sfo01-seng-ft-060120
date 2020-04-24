df9f76ff8eb3a4dc21cfcc7d21d2e23b0f36ffcc
Akiko Green <akikogreen08@gmail.com>
2020-04-24 00:22:03 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,4 @@
-# don't forget to add: require 'pry'
+ require 'pry'
 
 def get_user_input
   gets.chomp
@@ -12,10 +12,11 @@ end
 
 def selection(num)
   if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
+    return "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+    return "HAM HAM HAM IN MY TUMMY"
   end
+   binding.pry
 end
 
 def runner
